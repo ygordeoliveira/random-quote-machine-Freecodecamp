3fd179208589dfd9c0677af1eed402d64d67db58
Ygor de Oiveira Souza da Silva <ygor90928@gmail.com>
2023-09-20 17:01:18 -0300

    Make Bootstrap imports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
 import reportWebVitals from './reportWebVitals';
 import RandomQuoteMachine from './App';
 
